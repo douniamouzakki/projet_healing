d3a5ec9eea59fddc755b6b5eaa853d0c8a36facf
Dounia <dounia.mouzakki@gmail.com>
2023-12-17 15:27:26 -0800

    Création de la classe patient

---
+++ Hproject.py
@@ -0,0 +1,9 @@
+class Patient:
+    def __init__(self, nom, maladie, argent, poche, etat):
+        self.nom = nom
+        self.argent = argent
+        self.maladie = maladie 
+        self.poche = poche
+        self.etat = etat
+        self.lieu = "Salle d'attente"
+        
